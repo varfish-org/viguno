555cb46ee1d93bc7e32773950a8a22d76ca1612c
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2023-06-10 11:39:22 +0200

    wip

--- src/server/actix_server/hpo_omims.rs
+++ src/server/actix_server/hpo_omims.rs
@@ -10,7 +10,6 @@ use hpo::{
     term::HpoGroup,
     Ontology,
 };
-use serde::{Deserialize, Serialize};
 
 use crate::server::WebServerData;
 
@@ -29,7 +28,7 @@ use super::{CustomError, Match, ResultHpoTerm};
 /// The following propery defines how matches are performed:
 ///
 /// - `match` -- how to match
-#[derive(Deserialize, Debug, Clone)]
+#[derive(serde::Deserialize, Debug, Clone)]
 struct Request {
     /// The OMIM ID to search for.
     pub omim_id: Option<String>,
@@ -57,7 +56,7 @@ fn _default_hpo_terms() -> bool {
 }
 
 /// Result entry for `handle`.
-#[derive(Serialize, Debug, Clone)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 struct ResultEntry {
     /// The OMIM ID.
     pub omim_id: String,
@@ -68,6 +67,34 @@ struct ResultEntry {
     pub hpo_terms: Option<Vec<ResultHpoTerm>>,
 }
 
+impl PartialEq for ResultEntry {
+    fn eq(&self, other: &Self) -> bool {
+        (self.omim_id == other.omim_id) && (self.name == other.name)
+    }
+}
+
+impl Eq for ResultEntry {}
+
+impl PartialOrd for ResultEntry {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self.omim_id.partial_cmp(&other.omim_id) {
+            Some(core::cmp::Ordering::Equal) => {}
+            ord => return ord,
+        }
+        self.name.partial_cmp(&other.name)
+    }
+}
+
+impl Ord for ResultEntry {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.omim_id.cmp(&other.omim_id) {
+            core::cmp::Ordering::Equal => {}
+            ord => return ord,
+        }
+        self.name.cmp(&other.name)
+    }
+}
+
 impl ResultEntry {
     pub fn from_omim_disease_with_ontology(
         omim_disease: &OmimDisease,
@@ -75,24 +102,24 @@ impl ResultEntry {
         hpo_terms: bool,
     ) -> Self {
         let hpo_terms = if hpo_terms {
-            Some(
-                omim_disease
-                    .to_hpo_set(ontology)
-                    .child_nodes()
-                    .into_iter()
-                    .collect::<HpoGroup>()
-                    .into_iter()
-                    .map(|term_id| ontology.hpo(term_id))
-                    .filter(std::option::Option::is_some)
-                    .map(|term| {
-                        let term = term.expect("filtered above");
-                        ResultHpoTerm {
-                            term_id: term.id().to_string(),
-                            name: term.name().to_string(),
-                        }
-                    })
-                    .collect(),
-            )
+            let mut result = omim_disease
+                .to_hpo_set(ontology)
+                .child_nodes()
+                .into_iter()
+                .collect::<HpoGroup>()
+                .into_iter()
+                .map(|term_id| ontology.hpo(term_id))
+                .filter(std::option::Option::is_some)
+                .map(|term| {
+                    let term = term.expect("filtered above");
+                    ResultHpoTerm {
+                        term_id: term.id().to_string(),
+                        name: term.name().to_string(),
+                    }
+                })
+                .collect::<Vec<_>>();
+            result.sort();
+            Some(result)
         } else {
             None
         };
@@ -165,15 +192,100 @@ async fn handle(
         }
     }
 
+    result.sort();
+
     Ok(Json(result))
 }
 
 #[cfg(test)]
 mod test {
-    #[test]
-    fn test_handle() -> Result<(), anyhow::Error> {
-        assert!(false, "actually write the test");
+    /// Helper function for running a query.
+    #[allow(dead_code)]
+    async fn run_query(uri: &str) -> Result<Vec<super::ResultEntry>, anyhow::Error> {
+        let ontology = crate::common::load_hpo("tests/data/hpo")?;
+        let app = actix_web::test::init_service(
+            actix_web::App::new()
+                .app_data(actix_web::web::Data::new(crate::server::WebServerData {
+                    ontology,
+                    db: None,
+                }))
+                .service(super::handle),
+        )
+        .await;
+        let req = actix_web::test::TestRequest::get().uri(uri).to_request();
+        let resp: Vec<super::ResultEntry> =
+            actix_web::test::call_and_read_body_json(&app, req).await;
 
-        Ok(())
+        Ok(resp)
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_omim_id_exact_no_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?omim_id=616145").await?
+        ))
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_omim_id_exact_with_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?omim_id=616145&hpo_terms=true").await?
+        ))
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_name_exact_no_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?name=Catel-Manzke+syndrome").await?
+        ))
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_name_exact_with_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?name=Catel-Manzke+syndrome&hpo_terms=true").await?
+        ))
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_name_prefix_no_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?name=Catel-Manzke+syndro&match=prefix").await?
+        ))
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_name_prefix_with_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?name=Catel-Manzke+syndro&match=prefix&hpo_terms=true").await?
+        ))
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_name_suffix_no_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?name=tel-Manzke+syndrome&match=suffix").await?
+        ))
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_name_suffix_with_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?name=tel-Manzke+syndrome&match=suffix&hpo_terms=true").await?
+        ))
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_name_contains_no_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?name=tel-Manzke+syndro&match=contains").await?
+        ))
+    }
+
+    #[actix_web::test]
+    async fn hpo_omims_name_contains_with_hpo_terms() -> Result<(), anyhow::Error> {
+        Ok(insta::assert_yaml_snapshot!(
+            &run_query("/hpo/omims?name=tel-Manzke+syndro&match=contains&hpo_terms=true").await?
+        ))
     }
 }
