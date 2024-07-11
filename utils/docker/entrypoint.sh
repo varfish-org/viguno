f061c9df36a17c08d0d02d2c9ede169c9836cc84
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-07-11 15:14:55 +0200

    fix: remove path to hgnc_xlink.tsv from Docker entrypoint (#176)

--- utils/docker/entrypoint.sh
+++ utils/docker/entrypoint.sh
@@ -7,15 +7,12 @@ set -euo pipefail
 #
 #   PATH_HPO_DIR    -- path to the directory with HPO files
 #                      default: /data/hpo
-#   PATH_HGNC_XLINK -- path to the TSV xlink file
-#                      default: /data/hgnc_xlink.tsv
 #   HTTP_HOST       -- host to listen on
 #                      default: 0.0.0.0
 #   HTTP_PORT       -- port
 #                      default: 8080
 
 PATH_HPO_DIR=${PATH_HPO_DIR-/data/hpo}
-PATH_HGNC_XLINK=${PATH_HGNC_XLINK-/data/hgnc_xlink.tsv}
 HTTP_HOST=${HTTP_HOST-0.0.0.0}
 HTTP_PORT=${HTTP_PORT-8080}
 
@@ -28,7 +25,6 @@ else
   exec \
     viguno server run \
       --path-hpo-dir "$PATH_HPO_DIR" \
-      --path-hgnc-xlink "$PATH_HGNC_XLINK" \
       --listen-host "$HTTP_HOST" \
       --listen-port "$HTTP_PORT"
 fi
